6674c9b927c0e387ff8f5f47a14c088d16dcaf26
error-code <danielaisoajoseph@gmail.com>
2022-09-30 12:11:40 +0100

    Select all text field

--- verify-student.js
+++ verify-student.js
@@ -5,19 +5,23 @@ const create = document.querySelector('#create');
 const verifyMe = document.querySelector('#verifyMe');
 
 verify.addEventListener('click', () => {
-
+    const name = document.querySelector('#name');
+    const age = document.querySelector('#age');
+    const level = document.querySelector('#level');
 })
 
-createStudent.addEventListener('click', () => {
-    document.querySelector('.signin').style.display = 'none';
-    document.querySelector('.signup').style.display = 'block';
-});
+// createStudent.addEventListener('click', () => {
+//     document.querySelector('.signin').style.display = 'none';
+//     document.querySelector('.signup').style.display = 'block';
+// });
 
 create.addEventListener('click', () => {
-
+    const name = document.querySelector('#name');
+    const age = document.querySelector('#age');
+    const level = document.querySelector('#level');
 });
 
-verifyMe.addEventListener('click', () => {
-    document.querySelector('.signin').style.display = 'block';
-    document.querySelector('.signup').style.display = 'none';
-});
+// verifyMe.addEventListener('click', () => {
+//     document.querySelector('.signin').style.display = 'block';
+//     document.querySelector('.signup').style.display = 'none';
+// });
