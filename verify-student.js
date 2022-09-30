2e97490ea83af7e43aaa84055bb5210cd71dd3b2
error-code <danielaisoajoseph@gmail.com>
2022-09-30 12:05:36 +0100

    Add function to toggle between signin and signup

--- verify-student.js
+++ verify-student.js
@@ -3,3 +3,21 @@ const verify = document.querySelector('#verify');
 const createStudent = document.querySelector('#createStudent');
 const create = document.querySelector('#create');
 const verifyMe = document.querySelector('#verifyMe');
+
+verify.addEventListener('click', () => {
+
+})
+
+createStudent.addEventListener('click', () => {
+    document.querySelector('.signin').style.display = 'none';
+    document.querySelector('.signup').style.display = 'block';
+});
+
+create.addEventListener('click', () => {
+
+});
+
+verifyMe.addEventListener('click', () => {
+    document.querySelector('.signin').style.display = 'block';
+    document.querySelector('.signup').style.display = 'none';
+});
