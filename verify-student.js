c72e019fb7693dd8d0c4a3159657ab9670b3383d
error-code <danielaisoajoseph@gmail.com>
2022-10-01 22:52:00 +0100

    Function to check for the right input

--- verify-student.js
+++ verify-student.js
@@ -5,9 +5,67 @@ const create = document.querySelector('#create');
 const verifyMe = document.querySelector('#verifyMe');
 
 verify.addEventListener('click', () => {
+    // List data of Students
+    const nameLibrary = ["Error-Code"];
+    const ageLibrary = ["18"];
+    const levelLibrary = ["300"];
+
+    // Select all paragraphs field
+    const paraName = document.querySelector('.name');
+    const paraAge = document.querySelector('.age');
+    const paraLevel = document.querySelector('.level');
+
+    //Select all text box
     const name = document.querySelector('#name');
     const age = document.querySelector('#age');
     const level = document.querySelector('#level');
+    
+    const nameValue = name.value;
+    const ageValue = parseFloat(age.value);
+    const levelValue = parseFloat(level.value);
+
+    // console.log(nameValue);
+    // console.log(ageValue);
+    // console.log(levelValue);
+
+    if (!isNaN(nameValue)) {
+        errorAlert();
+    } else if (isNaN(ageValue)) {
+        errorAlert();
+    } else if (isNaN(levelValue)) {
+        errorAlert();
+    } else {
+        paraName.textContent = '';
+        paraAge.textContent = '';
+        paraLevel.textContent = '';
+    }
+    
+    function errorAlert() {
+        if (nameValue === "" || nameValue === undefined || nameValue === null) {
+            paraName.textContent = 'Please fill in your username!';
+        } else if (!isNaN(nameValue)) {
+            paraName.textContent = 'Username should begin with letters only!';
+        } else {
+            paraName.textContent = '';
+        }
+
+        if (isNaN(ageValue)) {
+            paraAge.textContent = 'age should contain numbers only!';
+            console.log(ageValue);
+        } else if (ageValue === null || ageValue === undefined) {
+            paraAge.textContent = 'please fill in your age!';
+        } else {
+            paraAge.textContent = '';
+        }
+
+        if (isNaN(levelValue)) {
+            paraLevel.textContent = 'level should contain numbers only!';
+        } else if (levelValue === " " || levelValue === undefined) {
+            paraLevel.textContent = 'please fill in your username!';
+        } else {
+            paraLevel.textContent = '';
+        }
+    }
 })
 
 // createStudent.addEventListener('click', () => {
@@ -16,6 +74,7 @@ verify.addEventListener('click', () => {
 // });
 
 create.addEventListener('click', () => {
+    // Select all text field
     const name = document.querySelector('#name');
     const age = document.querySelector('#age');
     const level = document.querySelector('#level');
