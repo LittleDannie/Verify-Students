ac0f5aeaa9e67d5732e1d30ef1b15180b3b75d79
error-code <danielaisoajoseph@gmail.com>
2022-10-02 05:59:23 +0100

    Function to create student account

--- verify-student.js
+++ verify-student.js
@@ -9,6 +9,7 @@ const nameLibrary = ["Error-Code"];
 const ageLibrary = ["18"];
 const levelLibrary = ["300"];
 
+// Verify button
 verify.addEventListener('click', () => {
     // Select all paragraphs field
     const paraName = document.querySelector('.name');
@@ -28,6 +29,7 @@ verify.addEventListener('click', () => {
     // console.log(ageValue);
     // console.log(levelValue);
 
+    // Verify if input matches
     if (!isNaN(nameValue)) {
         errorAlert();
     } else if (isNaN(ageValue)) {
@@ -40,7 +42,8 @@ verify.addEventListener('click', () => {
         paraLevel.textContent = '';
         checkLength();
     }
-    
+
+    // Check for the length of the inputs
     function checkLength() {
         nameValueLength = nameValue.length;
         ageValueLength = String(ageValue).length;
@@ -137,19 +140,121 @@ verify.addEventListener('click', () => {
     }
 })
 
+// Create Student Account button
 createStudent.addEventListener('click', () => {
     // Switches "become a student?" page
     document.querySelector('.signin').style.display = 'none';
     document.querySelector('.signup').style.display = 'block';
 });
 
+// Create button
 create.addEventListener('click', () => {
-    // Select all text field
-    const name = document.querySelector('#name');
-    const age = document.querySelector('#age');
-    const level = document.querySelector('#level');
+    // Select all paragraphs field
+    const paraName2 = document.querySelector('.name2');
+    const paraAge2 = document.querySelector('.age2');
+    const paraLevel2 = document.querySelector('.level2');
+
+    //Select all text box
+    const name2 = document.querySelector('#name2');
+    const age2 = document.querySelector('#age2');
+    const level2 = document.querySelector('#level2');
+    
+    const nameValue2 = name2.value;
+    const ageValue2 = parseFloat(age2.value);
+    const levelValue2 = parseFloat(level2.value);
+
+    // Verify if input matches
+    if (!isNaN(nameValue2)) {
+        errorAlert2();
+    } else if (isNaN(ageValue2)) {
+        errorAlert2();
+    } else if (isNaN(levelValue2)) {
+        errorAlert2();
+    } else {
+        paraName2.textContent = '';
+        paraAge2.textContent = '';
+        paraLevel2.textContent = '';
+        checkLength2();
+    }
+    
+    // Check for the length of the inputs
+    function checkLength2() {
+        nameValueLength2 = nameValue2.length;
+        ageValueLength2 = String(ageValue2).length;
+        levelValueLength2 = String(levelValue2).length;
+        let check12 = false;
+        let check22 = false;
+        let check32 = false;
+        nameCheck2();
+        ageCheck2();
+        levelCheck2();
+
+        if (check12 === true && check22 === true && check32 === true) {
+            storeRecord();
+        }
+
+        function nameCheck2() {
+            if (nameValueLength2 < 3) {
+                paraName2.textContent = 'Username should not be less than "3" characters!';
+            } else if (nameValueLength2 > 12) {
+                paraName2.textContent = 'Username should not be more than "12" characters'
+            } else {
+                check12 = true;
+            }
+        }
+
+        function ageCheck2() {
+            if (ageValueLength2 !== 2) {
+                    paraAge2.textContent = 'Age should not be more or less than 2 characters';
+            } else {
+                check22 = true;
+            }
+        }
+
+        function levelCheck2() {
+            if (levelValueLength2 !== 3) {
+                paraLevel2.textContent = 'Level should not be more or less than 3 characters';
+            } else {
+                check32 = true;
+            }
+        }
+    }
+
+    function storeRecord() {
+        nameLibrary.push(String(nameValue2));
+        ageLibrary.push(String(ageValue2));
+        levelLibrary.push(String(levelValue2));
+    }
+
+     // Validates characters and outputs error message
+     function errorAlert2() {
+        if (nameValue2 === "" || nameValue2 === undefined || nameValue2 === null) {
+            paraName2.textContent = 'Please fill in your username!';
+        } else if (!isNaN(nameValue2)) {
+            paraName2.textContent = 'Username should begin with letters only!';
+        } else {
+            paraName2.textContent = '';
+        }
+
+        if (isNaN(ageValue2)) {
+            paraAge2.textContent = 'age should contain numbers only!';
+        } else if (ageValue2 === null || ageValue2 === undefined) {
+            paraAge2.textContent = 'please fill in your age!';
+        } else {
+            paraAge2.textContent = '';
+        }
+
+        if (isNaN(levelValue2)) {
+            paraLevel2.textContent = 'level should contain numbers only!';
+        } else if (levelValue2 === " " || levelValue2 === undefined) {
+            paraLevel2.textContent = 'please fill in your username!';
+        } else {
+            paraLevel2.textContent = '';
+        }
+    }
 });
 
+// Verify Me button
 verifyMe.addEventListener('click', () => {
     // Switches to "are you a student?"" page
     document.querySelector('.signin').style.display = 'block';
